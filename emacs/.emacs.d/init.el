dd8fa6eb5bc12948d60b093a9b6bdfb4650b6164
Shaun Cheng <cheng754@gmail.com>
2025-09-25 14:15:41 -0700

    tab close SPC w

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -639,6 +639,7 @@ With C-u (PROMPT-DIRECTORY non-nil): Prompt for a directory and then run
     "g"   '(magit-status           :which-key "status")
     ;; new tab
     "t"  '(tab-new            :which-key "new tab")
+    "w"  '(tab-close            :which-key "close tab")
     "["  '(tab-bar-switch-to-prev-tab      :which-key "prev tab")
     "]"  '(tab-bar-switch-to-prev-tab      :which-key "next tab")
     
